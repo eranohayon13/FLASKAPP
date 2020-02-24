9b488a21ce26e7e5c92ea04bd9792512c6f8eebb
eranohayon13 <eohayon4@gmail.com>
2020-02-24 15:38:01 +0000

    updating

--- application/routes.py
+++ application/routes.py
@@ -2,12 +2,14 @@
 from flask import render_template
 # import the app object from the ./application/__init__.py
 from application import app
+from application.models import Posts
 
 # define routes for / & /home, this function will be called when these are accessed
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home', post=postData)
 
 @app.route('/about')
 def about():
